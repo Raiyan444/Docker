1d430f6b93d268fc7ce182782ce0188636b807cf
MOHAMED RAIYAN <speak2raiyan@gmail.com>
2020-04-28 14:08:48 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "**********************************"
 echo "*********Building JAR*************"
 echo "**********************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+#docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
 
-#docker run --rm -v /var/jenkins_home/workspace/pipeline-docker/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm -v ~/workspace/pipeline-docker/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
