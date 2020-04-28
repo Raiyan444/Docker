ff6134305c7382522e68b39ed5193d532880b294
Ubuntu <ubuntu@ip-192-168-0-30.ap-southeast-1.compute.internal>
2020-04-28 14:18:05 +0530

    updated path

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,7 @@ echo "**********************************"
 echo "*********Building JAR*************"
 echo "**********************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+WORKSPACE=/home/ubuntu/jenkins-data/jenkins_home/workspace/pipeline-docker
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
 
